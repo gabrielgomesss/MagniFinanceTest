0548f7998bad1e7af79a8dc5621646488e2eb83a
gabrielgomesss <gabriel.gomesss@hotmail.com>
2022-09-18 16:17:45 -0300

    [UPDATE][FIXING ROUTE]

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Routes from './Routes'
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes />
     </BrowserRouter>
   );
